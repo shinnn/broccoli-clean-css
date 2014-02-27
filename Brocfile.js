d16564bd068a5d227c028efa12e97ce258ba1c38
shinnn <snnskwtnb@gmail.com>
2014-02-28 00:37:46 +0900

    update test files and scripts

--- Brocfile.js
+++ Brocfile.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = function (broccoli) {
-  return require('./index')(broccoli.makeTree('test/fixture'));
+  return require('./index')(broccoli.makeTree('test/fixture'), {
+    keepSpecialComments: 1,
+    keepBreaks: true,
+    noAdvanced: true
+  });
 };
--- test/test.js
+++ test/test.js
@@ -3,11 +3,14 @@
 var assert = require('assert');
 var fs = require('fs');
 
-var expected = '#container{font:18px}';
+var expected = '/*! header */\n' +
+               'b{font:18px}\n' +
+               'b{color:red}';
 
 describe('broccoli-clean-css', function () {
   it('should run clean-css', function (done) {
-    fs.readFile('test/actual/src.css', function(err, data) {
+    fs.readFile('test/actual/simple.css', function(err, data) {
+      if (err) done(err);
       assert.strictEqual(data.toString(), expected);
       done();
     });

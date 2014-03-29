0c8d2a61a7cb400d098d9ab6c68fb3d72bff2f8c
shinnn <snnskwtnb@gmail.com>
2014-03-29 18:38:45 +0900

    fix coding style

--- test/test.js
+++ test/test.js
@@ -7,8 +7,8 @@ var expected = '/*! header */\n' +
                '#cnt{font:18px}\n' +
                '#cnt{background:url(bg.jpg)}';
 
-describe('broccoli-clean-css', function () {
-  it('should run clean-css', function (done) {
+describe('broccoli-clean-css', function() {
+  it('should run clean-css', function(done) {
     fs.readFile('test/actual/importer.css', function(err, data) {
       if (err) done(err);
       assert.strictEqual(data.toString(), expected);

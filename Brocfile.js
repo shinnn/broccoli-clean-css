e48e19f4f614339aab100dc94e1923107948ba77
shinnn <snnskwtnb@gmail.com>
2014-04-14 00:17:57 +0900

    update Broccoli to v0.9.0

--- Brocfile.js
+++ Brocfile.js
@@ -1,10 +1,8 @@
 'use strict';
 
-module.exports = function (broccoli) {
-  return require('./index')('test/fixture', {
+module.exports = require('./index')('test/fixture', {
     keepSpecialComments: 1,
     keepBreaks: true,
     noAdvanced: true,
     root: 'test/fixture'
   });
-};

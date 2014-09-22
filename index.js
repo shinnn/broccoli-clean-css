3bb40b5d9ac3efc8ea289936cd87392348e0f56b
Gil Pedersen <git@gpost.dk>
2014-09-22 13:37:17 +0200

    make root & relativeTo options relative to source tree
    previously the options were relative to the CWD

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var Filter = require('broccoli-filter');
 var CleanCSS = require('clean-css');
+var path = require('path');
 
 function CleanCSSFilter(inputTree, options) {
   if (!(this instanceof CleanCSSFilter)) {
@@ -10,6 +11,7 @@ function CleanCSSFilter(inputTree, options) {
 
   this.inputTree = inputTree;
   this.options = options || {};
+  this.cleaner = null;
 }
 
 CleanCSSFilter.prototype = Object.create(Filter.prototype);
@@ -18,8 +20,25 @@ CleanCSSFilter.prototype.constructor = CleanCSSFilter;
 CleanCSSFilter.prototype.extensions = ['css'];
 CleanCSSFilter.prototype.targetExtension = 'css';
 
+// add hook to read
+CleanCSSFilter.prototype.read = function(readTree) {
+  var self = this, args = arguments;
+
+  return readTree(this.inputTree).then(function (srcDir) {
+    // rewrite relative paths to resolve against the source tree
+    self.options.root = path.resolve(srcDir, self.options.root || '.');
+    if (self.options.relativeTo) {
+      self.options.relativeTo = path.resolve(srcDir, self.options.relativeTo);
+    }
+
+    self.cleaner = new CleanCSS(self.options);
+
+    return Filter.prototype.read.apply(self, args);
+  });
+};
+
 CleanCSSFilter.prototype.processString = function(str) {
-  return new CleanCSS(this.options).minify(str);
+  return this.cleaner.minify(str);
 };
 
 module.exports = CleanCSSFilter;

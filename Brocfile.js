7fb33025e6bff7a834196fd6fea98e51e40915bc
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-01-17 01:32:28 +0900

    test programmatically, instead of using CLI

--- Brocfile.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = require('./index')('test/fixtures', {
-  keepSpecialComments: 1,
-  keepBreaks: true,
-  noAdvanced: true
-});
--- test/test.js
+++ test/test.js
@@ -1,21 +1,83 @@
 'use strict';
 
-var assert = require('assert');
 var fs = require('fs');
+var path = require('path');
 
-var expected = '/*! header */\n' +
-               '#cnt{font:18px}\n' +
-               '#cnt{background:url(bg.jpg)}';
+var Builder = require('broccoli').Builder;
+var cleanCSS = require('..');
+var Funnel = require('broccoli-funnel');
+var rimraf = require('rimraf');
+var test = require('tape');
 
-describe('broccoli-clean-css', function() {
-  it('should run clean-css', function(done) {
-    fs.readFile('test/actual/importer.css', function(err, data) {
-      if (err) {
-        done(err);
-        return;
-      }
-      assert.strictEqual(data.toString(), expected);
-      done();
+rimraf.sync('tmp');
+
+test('broccoli-clean-css', function(t) {
+  t.plan(7);
+
+  new Builder(cleanCSS('test/fixtures')).build().then(function(dir) {
+    fs.readFile(path.join(dir.directory, 'importer.css'), 'utf8', function(err, content) {
+      t.deepEqual([err, content], [null, 'b{color:red}'], 'should minify CSS.');
     });
+  }, t.fail);
+
+  var tree = new Funnel('test/fixtures', {destDir: ''});
+  tree = cleanCSS(tree, {
+    sourceMap: true,
+    strict: true
+  });
+  tree = new Funnel('test/fixtures', {destDir: 'foo'});
+  tree = cleanCSS(tree, {
+    relativeTo: 'foo',
+    sourceMap: true,
+    strict: true
+  });
+
+  new Builder(tree).build().then(function(dir) {
+    fs.readFile(path.join(dir.directory, 'foo/importer.css'), 'utf8', function(err, content) {
+      t.strictEqual(err, null, 'should support `sourceMap` option.');
+      t.equal(
+        content.replace(/\/\*.*/, ''),
+        'b{color:red}\n\n',
+        'should resolve @import rules from the temporary directory.'
+      );
+      t.ok(
+        /\/\* # sourceMappingURL/.test(content),
+        'should append base64-encoded source map comment to the files.'
+      );
+    });
+  }, t.fail);
+
+  new Builder(cleanCSS('test/fixtures', {
+    keepSpecialComments: 1,
+    keepBreaks: true,
+    advanced: false,
+    root: 'test',
+    relativeTo: 'test/fixtures/nested'
+  })).build().then(function(dir) {
+    fs.readFile(path.join(dir.directory, 'importer.css'), 'utf8', function(err, content) {
+      var expected = [
+        '/*! header */',
+        'a{font:18px}',
+        'a{background:url(/fixtures/nested/bg.jpg)}'
+      ].join('\n');
+
+      t.deepEqual([err, content], [null, expected], 'should support clean-css options.');
+    });
+  }, t.fail);
+
+  var options = {
+    relativeTo: 'foo',
+    strict: true
+  };
+
+  new Builder(cleanCSS('test/fixtures', options)).build().catch(function(err) {
+    t.ok(
+      /Broken @import declaration/.test(err.message),
+      'should fail to minify CSS on error when `strict` option is enabled.'
+    );
+    t.deepEqual(options, {
+      relativeTo: 'foo',
+      strict: true
+    }, 'should not modify the original option object.');
   });
 });
